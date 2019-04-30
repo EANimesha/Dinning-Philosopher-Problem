c6c52a611ff35e8ff98e2515b7e51e2569838734
EANimesha <nimeshadilini999@gmail.com>
2019-04-30 23:30:37 +0530

    First push of code

---
+++ DiningProblem.java
@@ -0,0 +1,27 @@
+package Dine4_v1;
+
+import java.util.concurrent.Semaphore;
+
+
+public class DiningProblem {
+
+    private static final int n = 5;
+
+    public static void main(String[] args) {
+        Semaphore[] chopsticks = new Semaphore[n];
+
+        for (int i = 0; i < n; i++) {
+            chopsticks[i] = new Semaphore(1);
+        }
+
+        Philosopher[] philosophers = new Philosopher[n];
+
+        for (int i = 0; i < n; i++) {
+            philosophers[i] = new Philosopher(i, chopsticks[i], chopsticks[(i + 1) % n]);
+            new Thread(philosophers[i]).start();
+        }
+
+    }
+
+}
+
